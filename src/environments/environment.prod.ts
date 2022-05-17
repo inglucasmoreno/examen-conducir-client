696d9daf0993691a2a8da55f192f90670743ae31
Moreno Lucas Omar <ing.lucasmoreno@gmail.com>
2022-05-16 22:55:23 -0300

    Para servidor

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   base_url: 'https://equinocciotech.com',
-  // base_url: 'http://192.168.0.10:3000',
+  // base_url: 'http://192.168.0.12:3000',
 };
