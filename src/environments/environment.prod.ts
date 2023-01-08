bb3302dcfc0fe3df0204e5c299e013fd7ba33fcf
Lucas Omar Moreno <ing.lucasmoreno@gmail.com>
2023-01-08 17:51:34 -0300

    Para produccion local

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  base_url: 'http://45.178.3.232:3000',
-  // base_url: 'http://10.15.8.10:3001',
+  // base_url: 'http://45.178.3.232:3000',
+  base_url: 'http://10.15.8.10:3001',
 };
